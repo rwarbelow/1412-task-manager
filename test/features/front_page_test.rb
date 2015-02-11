458caf8e7a8062ec087703919cc97cc25541192b
Rachel Warbelow <rachelwarbelow@gmail.com>
2015-02-11 11:21:11 -0700

    change feature test

--- test/features/front_page_test.rb
+++ test/features/front_page_test.rb
@@ -2,6 +2,9 @@ require_relative '../test_helper'
 
 class CreateATaskTest < FeatureTest
   def test_user_can_create_a_task
+    TaskManager.create({ :title       => "a title", 
+                         :description => "a description",
+                         :id          => 1 })
     visit '/tasks'
     assert page.has_content?("All Tasks")
   end
