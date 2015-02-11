f42cb4737b56c7cc9173f89655c0baf0ed5e791f
Rachel Warbelow <rachelwarbelow@gmail.com>
2015-02-11 12:06:26 -0700

    done

--- test/features/front_page_test.rb
+++ test/features/front_page_test.rb
@@ -1,11 +1,9 @@
 require_relative '../test_helper'
 
-class CreateATaskTest < FeatureTest
-  def test_user_can_create_a_task
-    TaskManager.create({ :title       => "a title", 
-                         :description => "a description",
-                         :id          => 1 })
-    visit '/tasks'
-    assert page.has_content?("All Tasks")
+class FrontPageTest < FeatureTest
+  def test_user_sees_index_and_new_links
+    visit '/'
+    assert page.has_content?("Task Index")
+    assert page.has_content?("New Task")
   end
 end
